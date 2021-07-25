763c9c14e4c118efafdd4e311aba0e8b6507b826
dustin@altama.ca <dustin@altama.ca>
2021-07-25 01:07:37 -0700

    Added countdown for presale, and adjusted presale date

--- app/javascript/packs/profile_menu.js
+++ app/javascript/packs/profile_menu.js
@@ -1,9 +1,9 @@
 
 // Hide the alerts
-function hideLoadingDiv() {
-  setTimeout(function(){
-    document.getElementById('alert_container').classList.add('hidden');
-  },10000)
-}
+//function hideLoadingDiv() {
+//  setTimeout(function(){
+//    document.getElementById('alert_container').classList.add('hidden');
+//  },10000)
+//}
 
-window.onload = hideLoadingDiv;
+// window.onload = hideLoadingDiv;
